b4e4ffaa684b90afe5dc58113ba7ad1aee1c9b59
Mayak <gods@mast.com>
2023-12-28 19:38:02 +0400

    add db connect, update docker

--- src/data.rs
+++ src/data.rs
@@ -1,4 +1,26 @@
 pub mod data {
+    use sqlx::{postgres::PgPoolOptions, Postgres, Pool};
+    use tracing::error;
+    #[derive(Clone)]
+    pub struct AppState {
+        pub pool: Pool<Postgres>,
+    }
+    use crate::config::config::Config;
+    pub async fn connect(config: &Config) -> AppState {
+        let connect = PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&config.database.uri).await;
+        match connect{
+            Ok(db) => {
+                AppState{pool:db}
+            },
+            Err(err) => {
+                let error = format!("data - connection - can't connect to database | {}", err);
+                error!(error);
+                panic!("{}", error);
+            },
+        }
 
+    }
 
 }

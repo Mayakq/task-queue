4a83233af42c44ef5ec7303525af479d6c96b885
Mayak <gods@mast.com>
2023-12-28 22:43:50 +0400

    add "add_task"

--- src/app.rs
+++ src/app.rs
@@ -1,14 +1,51 @@
-pub mod app{
+pub mod app {
     use crate::{config::config::Config, data};
+
+    use crate::data::data::AppState;
+    use axum::extract::{Json, State};
+    use axum::http::StatusCode;
+    use axum::response::IntoResponse;
+    use axum::routing::post;
+    use axum::Router;
+    use serde::{Deserialize, Serialize};
     
-    use axum::{Router,  http::StatusCode, routing::get};
-    async fn healthy() -> StatusCode{
-        StatusCode::OK
+    use sqlx::{query, Executor};
+
+
+
+    #[derive(Deserialize, Serialize)]
+    struct CreateTask {
+        manager: String,
+        text: String,
     }
-    pub async fn start(config: &Config){
+
+    async fn add_tasks(
+        State(state): State<AppState>,
+        Json(payload): Json<CreateTask>,
+    ) -> impl IntoResponse {
+        let query = query(
+            r#"
+            insert into tasks (text, worker)
+            values ($1, (select id from manager where name = $2));"#,
+        )
+        .bind(payload.text)
+        .bind(payload.manager);
+        let result = state.pool.lock().await.execute(query).await;
+        match result {
+            Ok(_) => {
+                return StatusCode::CREATED;
+            }
+            Err(err) => {
+                println!("{:?}", err);
+            }
+        }
+        return StatusCode::BAD_REQUEST;
+    }
+
+    pub async fn start(config: &Config) {
         let app = Router::new()
-        .route("/", get(healthy))
-        .with_state(data::data::AppState::new(config).await.pool);
+            .route("/task", post(add_tasks))
+            .with_state(data::data::AppState::new(config).await);
         let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
         axum::serve(listener, app).await.unwrap();
     }

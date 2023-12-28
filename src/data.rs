b2bae1d1e559a8c83c7dd963f3b7bbe26aeb03ab
Mayak <gods@mast.com>
2023-12-28 22:44:22 +0400

    add Arc+Mutex AppState

--- src/data.rs
+++ src/data.rs
@@ -1,27 +1,32 @@
 pub mod data {
-    use sqlx::{postgres::PgPoolOptions, Postgres, Pool};
+    use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
+    use std::sync::Arc;
+    use tokio::sync::Mutex;
     use tracing::error;
+
     #[derive(Clone)]
     pub struct AppState {
-        pub pool: Pool<Postgres>,
+        pub pool: Arc<Mutex<Pool<Postgres>>>,
     }
+
     use crate::config::config::Config;
-    impl AppState{
-        pub async fn new(config: &Config) -> AppState{
+
+    impl AppState {
+        pub async fn new(config: &Config) -> Self {
             let connect = PgPoolOptions::new()
-            .max_connections(10)
-            .connect(&config.database.uri).await;
-            match connect{
-                Ok(db) => {
-                    AppState{pool:db}
+                .max_connections(10)
+                .connect(&config.database.uri)
+                .await;
+            match connect {
+                Ok(db) => Self {
+                    pool: Arc::new(Mutex::new(db)),
                 },
                 Err(err) => {
                     let error = format!("data - connection - can't connect to database | {}", err);
                     error!(error);
                     panic!("{}", error);
-                },
+                }
             }
-    
         }
     }
 }

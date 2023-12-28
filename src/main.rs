0d937b7c7d082a9d0b28598a4cef9495c4283c77
Mayak <gods@mast.com>
2023-12-28 18:43:08 +0400

    start server

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod config;
 mod data;
-
+mod app;
 use tracing::Level;
 
 #[tokio::main]
@@ -8,5 +8,5 @@ async fn main() {
     tracing_subscriber::fmt()
         .with_max_level(Level::TRACE)
         .init();
-    
+    app::app::start(&config::config::init_config().unwrap()).await;
 }
